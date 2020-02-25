8d967dc99c01226c078c1182f9bd3bf1c27c607a
Alex Soble <alex.soble@gsa.gov>
2020-02-25 16:31:23 -0600

    Pull NetIncome calculation into its own class

--- snap_financial_factors/tests/net_income_test.py
+++ snap_financial_factors/tests/net_income_test.py
@@ -1,29 +1,26 @@
 from snap_financial_factors.fetch_deductions import FetchDeductions
 
 class NetIncomeTest:
-    # TODO (ARS): Deductions beyond the standard deduction.
+    '''
+    Evaluates net income against the appropriate net income threshold.
+    '''
 
-    def __init__(self, input_data, deductions_data, income_limits):
-        # Load user input data
+    def __init__(self, input_data, net_income, income_limits):
         self.input_data = input_data
         self.state_or_territory = input_data['state_or_territory']
         self.monthly_income = input_data['monthly_income']
         self.household_size = input_data['household_size']
         self.household_includes_elderly_or_disabled = input_data['household_includes_elderly_or_disabled']
         self.resources = input_data['resources']
-
-        self.deductions_data = deductions_data
+        self.net_income = net_income
         self.income_limits = income_limits
 
     def calculate(self):
-        deductions = FetchDeductions(self.state_or_territory, self.household_size, self.deductions_data)
-        standard_deduction = deductions.standard_deduction()
         net_monthly_income_limit = self.income_limits.net_monthly_income_limit()
-        below_net_income_limit = (net_monthly_income_limit) > (self.monthly_income - standard_deduction)
+        below_net_income_limit = net_monthly_income_limit > self.net_income
 
         description = ['Net monthly income limit for state and household size: ${}'.format(net_monthly_income_limit)]
-        description.append('Standard deduction for state and household size: ${}'.format(standard_deduction))
-        description.append('Monthly income submitted to API: ${}'.format(self.monthly_income))
+        description.append('Net monthly income : ${}'.format(self.net_income))
         description.append('Eligibility factor -- Is household income (minus deductions) below net monthly income limit? {}.'.format(below_net_income_limit))
 
         return {
