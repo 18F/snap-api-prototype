d2c5d233fd9a28781d2065b6f0568fa7e7d509fd
Alex Soble <alex.soble@gsa.gov>
2020-02-27 15:46:32 -0600

    Work on more plain-language explanation

--- snap_financial_factors/net_income.py
+++ snap_financial_factors/net_income.py
@@ -25,54 +25,81 @@ class NetIncome:
         deductions_data = self.deductions_data
         monthly_job_income = self.monthly_job_income
         monthly_non_job_income = self.monthly_non_job_income
-        description = []
+        explanation = []
 
         # Add up income.
         total_income = monthly_job_income + monthly_non_job_income
+        income_explanation = (
+            "Let's start with total household income. " +
+            f"This household reports monthly earned income of ${monthly_job_income} " +
+            f"and additional monthly income of ${monthly_non_job_income}, " +
+            f"for a total income of ${total_income}."
+        )
+        explanation.append(income_explanation)
 
         # Add up deductions:
 
         # Standard deduction
         deductions = FetchDeductions(state_or_territory, household_size, deductions_data)
         standard_deduction = deductions.standard_deduction()
-        description.append('Standard deduction: ${}.'.format(standard_deduction))
+
+        standard_deduction_pdf_url = 'https://fns-prod.azureedge.net/sites/default/files/media/file/FY20-Maximum-Allotments-Deductions.pdf'
+        standard_deduction_explanation = (
+            "\nNext, we need to take into account deductions. " +
+            f"We start with a standard deduction of ${standard_deduction}. " +
+            f"<a class='why-link' href='{standard_deduction_pdf_url}'>why?</a>"
+        )
+        explanation.append(standard_deduction_explanation)
 
         # Earned income deduction
         earned_income_deduction = 0.2 * monthly_job_income
-        description.append('Earned income deduction: ${}.'.format(earned_income_deduction))
+        earned_income_deduction_explanation = (
+            "Next, we add the earned income deduction. " +
+            "This is equal to 20% of income from jobs or self-employment, " +
+            f"and comes to ${earned_income_deduction}."
+        )
+        explanation.append(earned_income_deduction_explanation)
 
         # Dependent care deduction
         dependent_care_deduction = self.dependent_care_costs
-        description.append('Dependent care deduction: ${}.'.format(dependent_care_deduction))
+        dependent_care_deduction_explanation = (
+            f"Next, we add a deduction for dependent care costs: ${dependent_care_deduction}."
+        )
+        explanation.append(dependent_care_deduction_explanation)
 
         # Medical expenses deduction
         medical_expenses_deduction = 0  # Set default
-
         if self.household_includes_elderly_or_disabled:
+            medical_deduction_explanation = "Next, deduct monthly medical expenses for elderly or disabled household members beyond $35. "
             if self.medical_expenses_for_elderly_or_disabled > 0:
                 if self.medical_expenses_for_elderly_or_disabled > 35:
                     medical_expenses_deduction = self.medical_expenses_for_elderly_or_disabled - 35
-                    description.append('Medical expenses deduction: ${}.'.format(medical_expenses_deduction))
+                    medical_deduction_explanation += f"Medical expenses deduction: ${medical_expenses_deduction}. "
                 else:
-                    description.append('Medical expenses are below the $35 monthly threshold for deduction.')
+                    medical_deduction_explanation += "In this case, medical expenses are below the $35 monthly threshold for deduction. "
             else:
-                description.append('Medical expenses: $0.')
+                medical_deduction_explanation += "In this case, there are no monthly medical expenses to deduct. "
+
+            explanation.append(medical_deduction_explanation)
 
         total_deductions = (standard_deduction +
                             earned_income_deduction +
                             dependent_care_deduction +
                             medical_expenses_deduction)
 
-        description.append('Total deductions: ${}.'.format(total_deductions))
+        explanation.append(f"The total of all deductions is ${total_deductions}. ")
 
         net_income = total_income - total_deductions
-        description.append('Total income: ${}.'.format(total_income))
 
         # Adjusted net income can't be negative
         if 0 > net_income:
             net_income = 0
 
-        description.append('Net income (total income minus deductions): ${}.'.format(net_income))
+        calculation_explanation = (
+            f"Total income ${total_income} minus total deductions {total_deductions} ." +
+            "equals net income: ${net_income}."
+        )
+        explanation.append(calculation_explanation)
 
         return {
             'result': net_income,
