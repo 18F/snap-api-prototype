fa6c4b3f19c1ec4e224aff6290a8048ffc31c423
Alex Soble <alex.soble@gsa.gov>
2020-03-18 11:17:02 -0500

    Add SUA data for IL and VT
    + Also add subfolder with CSV-to-YAML data, scripts, and README for more context and information

---
+++ program_data/standard_utility_allowances/sua_csv_to_yaml.py
@@ -0,0 +1,31 @@
+import csv
+import yaml
+
+data = {}
+
+reader = csv.DictReader(
+    open('Copy-of-SNAP-SUA-Table-FY2019.csv', newline=''),
+    fieldnames=[
+        'STATE_WITH_REGION',
+        'HEATING_AND_COOLING',
+        'BASIC_LIMITED_UTILITY_ALLOWANCE',
+        'ELECTRICITY',
+        'GAS_AND_FUEL',
+        'WATER',
+        'SEWAGE',
+        'TRASH',
+        'PHONE',
+    ])
+
+# State (with Region and Household Size)
+for index, row in enumerate(reader):
+    if index > 0:
+        print(row)
+        state_with_region = row['STATE_WITH_REGION']
+        row.pop('STATE_WITH_REGION')
+        data[state_with_region] = row
+
+yaml_data = yaml.dump(data)
+
+output_file = open('standard_utility_allowances_draft.yaml', 'r+')
+output_file.write(yaml_data)
