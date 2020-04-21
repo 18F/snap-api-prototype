b19a86915fe7314f3193e65d8596cf8da5fef5f9
Alex Soble <alex.soble@gsa.gov>
2020-04-21 17:00:32 -0500

    Create Python API entrypoint class for input data validation

---
+++ snap_financial_factors/benefit_estimate/snap_estimate_entrypoint.py
@@ -0,0 +1,34 @@
+from typing import Dict
+
+from snap_financial_factors.input_data.parse_input_data import ParseInputData
+from snap_financial_factors.benefit_estimate.benefit_estimate import BenefitEstimate
+
+
+class SnapEstimateEntrypoint:
+    '''
+    Entrypoint to the Python API.
+
+    Accepts input arguments. Passes to SnapBenefitEstimate if valid; otherwise, surfaces errors.
+
+    Returns a dict with a 'status' key which will be either 'OK' or 'ERROR'.
+    '''
+
+    def __init__(self, input_data: Dict) -> None:
+        self.input_data = input_data
+
+    def calculate(self):
+        parsed_input_data = ParseInputData(self.input_data).parse()
+
+        if parsed_input_data.valid is False:
+            return {
+                'errors': parsed_input_data.errors,
+                'status': 'ERROR',
+            }
+
+        input_data = parsed_input_data.result
+        benefit_estimate = BenefitEstimate(input_data).calculate()
+
+        return {
+            **benefit_estimate,
+            'status': 'OK',
+        }
