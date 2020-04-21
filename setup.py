6a88dde08700acb68dfc602cb6b6f638a2caf990
Alex Soble <alex.soble@gsa.gov>
2020-04-21 11:27:15 -0500

    Back out MANIFEST.in

--- setup.py
+++ setup.py
@@ -20,6 +20,5 @@ setuptools.setup(
     python_requires='>=3.8.1',
     install_requires=[
         'PyYAML >= 5.3'
-    ],
-    include_package_data=True,  # Checks MANIFEST.in for explicit rules
+    ]
 )
