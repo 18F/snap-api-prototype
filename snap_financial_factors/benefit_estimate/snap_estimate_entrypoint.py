cba340665e9f1999a2b16d21d7a41b83b558097d
Alex Soble <alex.soble@gsa.gov>
2020-04-22 16:48:06 -0500

    Revert "Introduce unknown key to trigger typechecking error"
    This reverts commit a63afc9e4dd83d9803a9e28879f80d8f0af9416f.

--- snap_financial_factors/benefit_estimate/snap_estimate_entrypoint.py
+++ snap_financial_factors/benefit_estimate/snap_estimate_entrypoint.py
@@ -41,7 +41,6 @@ class SnapEstimateEntrypoint:
         result = BenefitEstimate(input_data).calculate()
 
         return {
-            'lets_see_if_tests_go': 'red',
             'status': 'OK',
             'eligible': result['eligible'],
             'estimated_monthly_benefit': result['estimated_monthly_benefit'],
